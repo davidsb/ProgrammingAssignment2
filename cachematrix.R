0239ce9a1e33bc06ae42c61c1d53556f493c19bc
David Starks-Browning <davidsb@users.noreply.github.com>
2014-08-17 21:30:34 +0100

    code (not tested)

--- cachematrix.R
+++ cachematrix.R
@@ -12,9 +12,18 @@
 ## 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
-
 
 ## cacheSolve computes the inverse of the special "matrix" created
 ## with makeCacheMatrix.  However, it first checks to see if the
@@ -25,4 +34,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if (!is.null(inv)) {
+                message("getting cached data")
+                return (inv)
+        }
+        # Necessary to compute the inverse here.
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
